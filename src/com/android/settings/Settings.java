b7fc5672aded74398bc868838d1e152125e2248a
maxwen <max.weninger@gmail.com>
2017-09-08 05:07:18 +0530

    Build PixysSettings into Settings
      * Add it on dashboard
      * Set proper priority to show it below Apps & Notifications
    
    Change-Id: I8e5e49802afe71bfbdb6cb014b749f8a75fd2ebe
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -178,4 +178,6 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // PixysOS Additions.
+    public static class PixysSettingsActivity extends SettingsActivity {}
 }
