5f8d230a8cc0ab9801b7539c8f50c87b629eb632
maxwen <max.weninger@gmail.com>
2018-02-10 02:58:13 -0500

    Configure SystemUI theme
    Allow to force light or dark or use the wallpaper based
    automatic selection
    
    [Kshitij Gupta]
    - Add a preference controller so as to adapt code for Display Settings
    - Refactor some variables. We're theming System, not just SystemUI
    
    Change-Id: Ia2361b9db7009e2a3806b65b22c7e0532e6ebeed

--- src/com/android/settings/display/DarkUIPreferenceController.java
+++ src/com/android/settings/display/DarkUIPreferenceController.java
@@ -16,28 +16,24 @@ package com.android.settings.display;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.os.Bundle;
-import android.content.Intent;
 import android.text.TextUtils;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
-import com.android.settings.R;
 import com.android.settingslib.core.AbstractPreferenceController;
-import com.android.settings.core.PreferenceControllerMixin;
 
 import libcore.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
-import android.widget.Toast;
 
 
 public class DarkUIPreferenceController extends AbstractPreferenceController implements
-        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener {
 
-    private static final String SYSTEM_THEME_STYLE = "system_theme_style";
-    private ListPreference mSystemThemeStyle;
+    private static final String SYSTEM_UI_THEME = "systemui_theme_style";
+    private ListPreference mSystemUiThemeStyle;
 
     public DarkUIPreferenceController(Context context) {
         super(context);
@@ -45,7 +41,7 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
 
     @Override
     public String getPreferenceKey() {
-        return SYSTEM_THEME_STYLE;
+        return SYSTEM_UI_THEME;
     }
 
     @Override
@@ -56,28 +52,23 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME_STYLE);
-        int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_THEME_STYLE, 0);
-        int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
-        mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
-        mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        mSystemUiThemeStyle = (ListPreference) screen.findPreference(SYSTEM_UI_THEME);
+        int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SYSTEM_UI_THEME, 0);
+        int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
+        mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
+        mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
     }
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mSystemThemeStyle) {
+        if (preference == mSystemUiThemeStyle) {
             String value = (String) newValue;
-            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SYSTEM_THEME_STYLE, Integer.valueOf(value));
-            int valueIndex = mSystemThemeStyle.findIndexOfValue(value);
-            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntries()[valueIndex]);
-            Toast.makeText(mContext, mContext.getString(R.string.ui_style_applied_toast),
-                Toast.LENGTH_LONG).show();
-            Intent goHome = new Intent(Intent.ACTION_MAIN);
-            goHome.addCategory(Intent.CATEGORY_HOME);
-            goHome.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(goHome);
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SYSTEM_UI_THEME, Integer.valueOf(value));
+            int valueIndex = mSystemUiThemeStyle.findIndexOfValue(value);
+            mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntries()[valueIndex]);
         }
         return true;
     }
 }
+
