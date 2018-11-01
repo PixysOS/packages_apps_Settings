58d2b69592e55a5e3236c54cb59d93cf53c09869
Alex Cruz <alex@dirtyunicorns.com>
2018-11-01 00:44:05 -0500

    Beautify QS tile styles picker [1/2]
    Change-Id: Ic2128c096d11df95be0dd71269035fcc02f4510f

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -35,7 +35,7 @@ import com.android.settings.display.ColorModePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
-import com.android.settings.display.QsTileStylePreferenceController;
+import com.android.settings.display.QsTileStylesPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -65,6 +65,7 @@ public class DisplaySettings extends DashboardFragment {
 
     private IntentFilter mIntentFilter;
     private static FontPickerPreferenceController mFontPickerPreference;
+    private static QsTileStylesPreferenceController mQsTileStylesPreference;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
@@ -132,7 +133,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new NightDisplayPreferenceController(context));
         controllers.add(new NightModePreferenceController(context));
-        controllers.add(new QsTileStylePreferenceController(context));
+        controllers.add(mQsTileStylesPreference = new QsTileStylesPreferenceController(context, lifecycle, fragment));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new AmbientDisplayPreferenceController(
                 context,
