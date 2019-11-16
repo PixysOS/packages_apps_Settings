f08bcc20abe8e7dd524b0861e2ac4f9d19a184e4
Pranav Vashi <neobuddy89@gmail.com>
2019-11-17 02:30:00 +0530

    Settings: Bring back Gaming mode [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Subinsmani <subins.mani@gmail.com>
    Change-Id: I134f0242d6e12d9a04e2e2644cd820d5dd96f137

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -179,4 +179,5 @@ public class Settings extends SettingsActivity {
 
     // PixysOS Additions.
     public static class PixysSettingsActivity extends SettingsActivity {}
+    public static class GamingModeActivity extends SettingsActivity {}
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,6 +149,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.pixys.settings.PixysSettings;
+import com.pixys.settings.fragments.GamingMode;
 
 public class SettingsGateway {
 
@@ -286,7 +287,8 @@ public class SettingsGateway {
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
-            PixysSettings.class.getName()
+            PixysSettings.class.getName(),
+            GamingMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -326,5 +328,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.PixysSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }
