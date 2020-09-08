ezio84/abc-n_packages_apps_Settings

Hide notification light color preference if no multicolor led
on and off time will be still available instead

src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -55,7 +55,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             return false;
         }
         return checkCanBeVisible(NotificationManager.IMPORTANCE_DEFAULT)
-                && canPulseLight();
+                && canPulseLight() && isMultiColorLed();
     }
 
     public void updateState(Preference preference) {
@@ -88,6 +88,11 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
+    }
+
+    boolean isMultiColorLed() {
+        return mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_multiColorBatteryLed);
     }
 
     private void showLedPreview() {

PixysOS/packages_apps_Settings

Change-Id: I0bcc8d7f788673a7a7b5ea762540e507c2f10965

src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -55,7 +55,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             return false;
         }
         return checkCanBeVisible(NotificationManager.IMPORTANCE_DEFAULT)
-                && canPulseLight();
+                && canPulseLight() && isMultiColorLed();
     }
 
     public void updateState(Preference preference) {
@@ -88,6 +88,11 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
+    }
+
+    boolean isMultiColorLed() {
+        return mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_intrusiveNotificationLed);
     }
 
     private void showLedPreview() {