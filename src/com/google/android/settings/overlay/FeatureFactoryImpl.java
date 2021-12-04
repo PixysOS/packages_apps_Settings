fb40d0becf7534084f9a133deebfb3e45a3fae85
Anay Wadhera <anay1018@gmail.com>
2021-12-04 14:07:55 -0500

    Settings: Import AccountFeatureProvider
    Change-Id: I00aa32f82b2bbc8cc2dd6d6d8079ec1436b93ac0
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,11 +1,24 @@
 package com.google.android.settings.overlay;
 
+import android.content.Context;
+
+import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.applications.GameSettingsFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 import com.google.android.settings.games.GameSettingsFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
+
+    private AccountFeatureProvider mAccountFeatureProvider;
     private GameSettingsFeatureProvider mGameSettingsFeatureProvider;
+
+    @Override
+    public AccountFeatureProvider getAccountFeatureProvider() {
+        if (mAccountFeatureProvider == null) {
+            mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
+        }
+        return mAccountFeatureProvider;
+    }
 
     @Override
     public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
