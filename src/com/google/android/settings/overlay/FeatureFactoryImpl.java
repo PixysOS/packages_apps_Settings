9b04539c23d3fba0131edac700e8160e8fc1be4e
aswin7469 <aswinas@pixysos.com>
2022-05-03 16:26:05 +0530

    Revert "FeatureFactoryImpl: Implement AccountFeatureProviderGoogleImpl"
    This reverts commit ed38893f49f3719922584b368d1d42a2ce4fe9ec.
    
    Change-Id: Idbd804710a85ff46a20d77bff5c68d6d37108276

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,21 +1,11 @@
 package com.google.android.settings.overlay;
 
-import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.applications.GameSettingsFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 import com.google.android.settings.games.GameSettingsFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
-    private AccountFeatureProvider mAccountFeatureProvider;
     private GameSettingsFeatureProvider mGameSettingsFeatureProvider;
-
-    @Override
-    public AccountFeatureProvider getAccountFeatureProvider() {
-        if (mAccountFeatureProvider == null) {
-            mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
-        }
-        return mAccountFeatureProvider;
-    }
 
     @Override
     public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
