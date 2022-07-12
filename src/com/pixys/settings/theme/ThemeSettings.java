39de2a09adb843862d201131203f4258e4252402
vjspranav <pranavasri@live.in>
2022-07-13 01:47:39 +0200

    Settings: Add toggle for Custom UI [2/2]
    Change-Id: I541f18ff25280f7b117aeb949a14f2efc1ab5b5c
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- src/com/pixys/settings/theme/ThemeSettings.java
+++ src/com/pixys/settings/theme/ThemeSettings.java
@@ -6,13 +6,29 @@
 
 package com.pixys.settings.theme;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.*;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
+import com.pixys.settings.preferences.CustomUIPreferenceController;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
@@ -33,6 +49,18 @@ public class ThemeSettings extends DashboardFragment {
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.theme_customization_settings;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new CustomUIPreferenceController(context));
+        return controllers;
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
