43fe05e9e0b592d86e9b14be3d72c330a3475546
aswin7469 <aswinas@pixysos.com>
2023-01-13 21:27:04 +0530

    Settings: Move Theming to settings homepage
    * cherry-picked from https://github.com/PixysOS/packages_apps_Settings@8eab284f12401acbd4f8b64d8a17775e41f7163e
    
    Change-Id: Iee80b3cb415313ed25d3da66002cc6a9ed1cc5fa
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -22,6 +22,7 @@ import com.android.settings.IccLockSettings;
 import com.android.settings.MainClear;
 import com.android.settings.MainClearConfirm;
 import com.android.settings.Settings;
+import com.pixys.settings.theme.ThemeSettings;
 import com.android.settings.TestingSettings;
 import com.android.settings.TetherSettings;
 import com.android.settings.TrustedCredentialsSettings;
@@ -351,7 +352,8 @@ public class SettingsGateway {
             TurnScreenOnSettings.class.getName(),
             TurnScreenOnDetails.class.getName(),
             WifiTetherClientManager.class.getName(),
-            SecurityHubDashboard.class.getName()
+            SecurityHubDashboard.class.getName(),
+            ThemeSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
