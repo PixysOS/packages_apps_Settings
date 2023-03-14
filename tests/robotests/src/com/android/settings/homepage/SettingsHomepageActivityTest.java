80ec7252544f5d4a2147ebb9037242df0853ac36
aswin7469 <aswinas@pixysos.com>
2023-03-14 20:55:56 +0530

    Revert "Remove BatteryFixSlice as it's not used"
    This reverts commit 2f80e073d0cfaca6098d8dc7e76542d6cc7d9256.
    
    Change-Id: I5b012ed05541c08599ef2ba11e2a54902fdea0b5

--- tests/robotests/src/com/android/settings/homepage/SettingsHomepageActivityTest.java
+++ tests/robotests/src/com/android/settings/homepage/SettingsHomepageActivityTest.java
@@ -41,6 +41,7 @@ import androidx.fragment.app.Fragment;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.suggestions.SuggestionFeatureProviderImpl;
+import com.android.settings.homepage.contextualcards.slices.BatteryFixSliceTest;
 import com.android.settings.testutils.shadow.ShadowActivityEmbeddingUtils;
 import com.android.settings.testutils.shadow.ShadowPasswordUtils;
 import com.android.settings.testutils.shadow.ShadowUserManager;
@@ -159,6 +160,9 @@ public class SettingsHomepageActivityTest {
     }
 
     @Test
+    @Config(shadows = {
+            BatteryFixSliceTest.ShadowBatteryTipLoader.class
+    })
     public void onStart_isNotDebuggable_shouldHideSystemOverlay() {
         ReflectionHelpers.setStaticField(Build.class, "IS_DEBUGGABLE", false);
 
@@ -175,6 +179,9 @@ public class SettingsHomepageActivityTest {
     }
 
     @Test
+    @Config(shadows = {
+            BatteryFixSliceTest.ShadowBatteryTipLoader.class,
+    })
     public void onStop_isNotDebuggable_shouldRemoveHideSystemOverlay() {
         ReflectionHelpers.setStaticField(Build.class, "IS_DEBUGGABLE", false);
 
