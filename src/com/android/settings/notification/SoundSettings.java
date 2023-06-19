1f0776ac0e4e79075f2c3f14efaeb3df0c2f8241
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-19 22:27:28 +0300

    Settings: Allow separating ringer & notification sound streams
    Using the newly added device config (QPR3).
    AOSP have already implemented listeners for this to be runtime,
    but it seems like they never tested settings side properly - especially when the toggle is at the same page.
    After fixing some funny logic errors it now updates just fine.
    base side needs no extra modifications, the observers there are programmed correctly.
    
    Change-Id: I0677e60674bc0d6dc6384fcc88dae816006eed9b
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -39,6 +39,7 @@ import com.android.settings.core.OnActivityResultListener;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.sound.HandsFreeProfileOutputPreferenceController;
+import com.android.settings.sound.SeparateNotificationPreferenceController;
 import com.android.settings.sound.VolumeDialogPositionPreferenceController;
 import com.android.settings.widget.PreferenceCategoryController;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -277,7 +278,10 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
         final VolumeDialogPositionPreferenceController volumeDialogPositionPreferenceController =
                 new VolumeDialogPositionPreferenceController(context);
+        final SeparateNotificationPreferenceController separateNotificationPreferenceController =
+                new SeparateNotificationPreferenceController(context);
 
+        controllers.add(separateNotificationPreferenceController);
         controllers.add(volumeDialogPositionPreferenceController);
         controllers.add(dialPadTonePreferenceController);
         controllers.add(inCallNotificationsPreferenceController);
